8a84122d7b99208d7f300db231406407432ab686
suppergerrie2 <gerjan2001@gmail.com>
2018-12-16 16:45:56 +0100

    Some cleaning

--- src/main/java/com/suppergerrie2/ai/commands/CommandCreateRoom.java
+++ src/main/java/com/suppergerrie2/ai/commands/CommandCreateRoom.java
@@ -3,25 +3,28 @@ package com.suppergerrie2.ai.commands;
 import com.suppergerrie2.ChaosNetClient.components.TrainingRoom;
 import com.suppergerrie2.ai.MinecraftAI;
 import net.minecraft.command.CommandBase;
-import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
 
+import javax.annotation.Nonnull;
+
 public class CommandCreateRoom extends CommandBase {
 
     @Override
+    @Nonnull
     public String getName() {
         return "createroom";
     }
 
     @Override
-    public String getUsage(ICommandSender sender) {
+    @Nonnull
+    public String getUsage(@Nonnull ICommandSender sender) {
         return "/createroom roomname namespace simModelNamespace";
     }
 
     @Override
-    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+    public void execute(@Nonnull MinecraftServer server, @Nonnull ICommandSender sender, @Nonnull String[] args) {
         if (args.length != 3) {
             //TODO: Translatable
             sender.sendMessage(new TextComponentString(this.getUsage(sender)));
